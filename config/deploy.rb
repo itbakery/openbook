7ab19b607dc56b4c82d105db2a1453199e0b4449
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-04 00:14:11 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ namespace :deploy do
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path} && rake db:seed RAILS_ENV=#{rails_env}"
   end
 
   desc "Update the necessary prerequisite"
