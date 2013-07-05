8857c44bb1d278bf4f36101c9c7e1c21414bac81
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-05 14:39:55 +0700

    commit

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,12 @@
 
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
-  :port                 => 25,
+  :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "thaiopensource@gmail.com",
-  :password             => "s,uiydgvb',kd"
+  :password             => "s,uiydgvb',kd",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
 }
 
 #require 'development_mail_interceptor'
