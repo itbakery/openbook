4bd19a1545f64b946c6d20bd21fa59070c6ccfa5
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-05 15:16:12 +0700

    commit

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,14 +1,17 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "thaiopensource@gmail.com",
+  :user_name            => "sawangpongm@gmail.com",
   :password             => "s,uiydgvb',kd",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
 #require 'development_mail_interceptor'
-#ActionMailer::Base.default_url_options[:host] = "sitourtravel.com"
+ActionMailer::Base.default_url_options[:host] = "sitourtravel.com"
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
