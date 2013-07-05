a65a55a42b43982ab3877a936adb3b6002be17f0
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-05 16:31:22 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,6 @@ Openbook::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-
+  config.active_support.deprecation  :notify
   # Send deprecation notices to registered listeners
 end
