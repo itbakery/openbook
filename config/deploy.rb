1e20c3007b6bbdac259810cf9b082404abd9904d
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-12 04:41:29 +0700

    Commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,10 @@ set :scm_verbose,true
 set :branch, "master"
 
 #==== intetration with capistrano
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 #set :rvm_type, :system
-#set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
-set :rvm_ruby_string, 'ruby-1.9.3-p429@openbook'
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+#set :rvm_ruby_string, 'ruby-1.9.3-p429@openbook'
 #set :rvm_ruby_string, 'ruby-1.9.3'
 set :rvm_type, :user
 set :rvm_bin_path, "/home/deploy/.rvm/bin"
