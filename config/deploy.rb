a331796ccb5ba068e2564e088e9008647d0e3aa0
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-12 14:31:59 +0700

    comit

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :branch, "master"
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 #set :bundle_cmd , "/home/deploy/.rvm/gems/ruby-1.9.3-p194@mrtablueline/bin/bundle"
 set :rvm_type, :user
-set :rvm_bin_path, "/home/deploy/.rvm/rubies/ruby-1.9.3-p429/bin/"
+set :rvm_bin_path, "/home/deploy/.rvm/bin/"
 #============
 
 ssh_options[:forward_agent] = true
