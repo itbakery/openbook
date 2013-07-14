f172c5d62196730d28b3dab1f6826072247c624f
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-15 00:13:25 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,5 @@ Openbook::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation  :notify
   # Send deprecation notices to registered listeners
-  config.assets.precompile += %w(Ckeditor.assets)
 
 end
