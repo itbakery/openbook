32c587e4c054bd77773945bd1555dab7be0f15d5
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-16 16:56:01 +0700

    commit

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,7 @@ module Openbook
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+
   end
 end
