8aa495bb073e7bfd8063b32fe381e8ee756a0381
sawangpong muadphet <sawangpongm@gmail.com>
2013-07-19 14:53:10 +0700

    commit

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module Openbook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
   end
